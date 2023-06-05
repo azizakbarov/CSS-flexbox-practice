e4108b8f7db2a64e285fb94f99ee1f0084ca7459
Aziz Akbarov <azizbakbarov@gmail.com>
2023-06-05 12:16:05 +0500

    JS Control Flow

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,60 @@
+// // // // // for (let i = 0; i < 5; i++) {
+// // // // //   console.log("in loop");
+// // // // // }
+
+// // // // const names = ["shaun", "luigi", "alex"];
+
+// // // // // for (let i = 0; i < names.length; i++) {
+// // // // //   console.log(names[i]);
+// // // // // }
+
+// // // // let i = 0;
+// // // // // while (i < 5) {
+// // // // //   console.log("in loop", i);
+// // // // //   i++;
+// // // // // }
+
+// // // // while(i < names.length){
+// // // //     console.log(names[i]);
+// // // //     i++;
+// // // // }
+// // // let i = 5;
+
+// // // do {
+// // //   console.log("the val of i is", i);
+// // //   i++;
+// // // } while (i < 5);
+// // const password = "passs";
+
+// // if (password.length >= 8) {
+// //   console.log("your password is long enough");
+// // } else {
+// //   console.log("your password is not long enough");
+// // }
+
+// const grade = "A"
+
+// switch(grade){
+//     case 'A':
+//     console.log("You got A")
+//     break
+//     case 'B':
+//     console.log("You got B")
+//     break
+//     case 'C':
+//     console.log("You got C")
+//     break
+//     case 'D':
+//     console.log("You got D")
+//     break
+//     case 'E':
+//     console.log("You got E")
+//     break
+//     default:
+//     console.log('Not a valid grade')
+// }
+
+const speak = function (name) {
+  console.log(`Good day ${name}`);
+};
+speak('Aziz');
