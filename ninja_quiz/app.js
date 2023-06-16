84d5e0fe5cdf97b407993c79c543729a6b7e2354
Aziz Akbarov <azizbakbarov@gmail.com>
2023-06-16 11:08:34 +0500

    Animating the quiz

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -13,10 +13,19 @@ form.addEventListener('submit', e => {
             score += 25;
         }
     })
-    if(score){
-
-    }
-
-    result.querySelector('span').textContent= `${score}%`;
+  
+    scrollTo(0,0);
+    
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent= `${output}%`;
+        if (output === score){
+            clearInterval(timer)
+        } else {
+            output++; 
+        }
+    }, 10);
 })
+
