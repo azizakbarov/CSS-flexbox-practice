261a7a5f129ea0c69b98e6de7bcd381eae9b17a6
Aziz Akbarov <azizbakbarov@gmail.com>
2023-06-19 17:22:27 +0500

    Practicing Event listeners

--- app.js
+++ app.js
@@ -1,54 +1,16 @@
-const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos');
-const search= document.querySelector('.search input');
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close');
 
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
 
-const generateTemplate = todo => {
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
 
-    const html =`
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${todo}</span>
-        <i class="far fa-trash-alt delete"></i>
-    </li>`
-    list.innerHTML += html;
-}
+popup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
 
-
-addForm.addEventListener('submit', e=>{
-    e.preventDefault();
-    const todo = addForm.add.value.trim();
-
-    if(todo.length){  
-    generateTemplate(todo);
-    addForm.reset();
-}
-  
-})
-
-// delete todos
-
-list.addEventListener('click', e=>{
-
-    if (e.target.classList.contains('delete')){
-        e.target.parentElement.remove();
-    }
-})
-
-//keyup event 
-
-const filteredTodos = (term) => {
-   
-    Array.from(list.children)
-    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
-    .forEach((todo) => todo.classList.add('filtered'));
-
-    Array.from(list.children)
-    .filter((todo) => todo.textContent.toLowerCase().includes(term))
-    .forEach((todo) => todo.classList.remove('filtered'));
-
-}
-
-search.addEventListener('keyup' , () => {
-    const term = search.value.trim().toLowerCase();
-    filteredTodos(term);
-})
